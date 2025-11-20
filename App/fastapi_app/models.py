0abd5990916a735d6e99bf4984e2d3181f8c7429
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-11-20 16:41:03 -0500

    Update models.py

--- App/fastapi_app/models.py
+++ App/fastapi_app/models.py
@@ -3,11 +3,12 @@
 from typing import List, Optional
 
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, Session
+from sqlalchemy.orm import declarative_base, Session, relationship
 from pydantic import BaseModel
 from datetime import date
 from datetime import datetime
 from enum import Enum
+
 
 Base = declarative_base()
 
