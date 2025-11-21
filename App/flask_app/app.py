cd007d042c974c1e79d6ec84be3e5121a0b63cc5
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-11-21 10:18:11 -0500

    Update app.py
    importing flash

--- App/flask_app/app.py
+++ App/flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request, redirect, url_for, session
+from flask import Flask, render_template_string, request, redirect, url_for, session, flash
 import requests
 import os
 
