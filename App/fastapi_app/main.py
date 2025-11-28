2acc71afee16a4027738aa1e7d950db53de0cc61
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-11-27 20:26:18 -0500

    Update main.py
    adding conditional

--- App/fastapi_app/main.py
+++ App/fastapi_app/main.py
@@ -81,7 +81,7 @@ def login(
     user = get_user_by_username(db, form_data.username)
     if user is None or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Usuario o contraseña incorrectos",
         )
 
