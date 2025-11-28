e77fa400d4d5368abd7c0544f3571d8340c33628
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-11-27 19:55:49 -0500

    Update main.py
    adding PSE user

--- App/fastapi_app/main.py
+++ App/fastapi_app/main.py
@@ -81,7 +81,7 @@ def login(
     user = get_user_by_username(db, form_data.username)
     if user is None or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Usuario o contraseña incorrectos",
         )
 
@@ -90,6 +90,7 @@ def login(
         "access_token": access_token,
         "token_type": "bearer",
         "username": user.username,
+        "user_id": user.id,
     }
 
 
