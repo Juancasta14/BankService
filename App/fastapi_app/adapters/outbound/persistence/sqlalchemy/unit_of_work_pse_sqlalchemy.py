f4dfb64de49b17f706f46a0434e9d8067d5e8bb3
Juan David <juanfelipe9902@gmail.com>
2026-02-08 17:11:07 -0500

    arregrando imports

--- App/fastapi_app/adapters/outbound/persistence/sqlalchemy/unit_of_work_pse_sqlalchemy.py
+++ App/fastapi_app/adapters/outbound/persistence/sqlalchemy/unit_of_work_pse_sqlalchemy.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Session
-from .accounts_repository_sqlalchemy import AccountRepositorySqlAlchemy
+from .accounts_repository_sqlalchemy import AccountsRepositorySqlAlchemy
 from .pse_transaction_repository_sqlalchemy import PSETransactionRepositorySqlAlchemy
 from .movement_repository_sqlalchemy import MovementRepositorySqlAlchemy
 
 class SqlAlchemyPSEUnitOfWork:
     def __init__(self, db: Session):
         self.db = db
-        self.accounts = AccountRepositorySqlAlchemy(db)
+        self.accounts = AccountsRepositorySqlAlchemy(db)
         self.pse = PSETransactionRepositorySqlAlchemy(db)
         self.movements = MovementRepositorySqlAlchemy(db)
 
