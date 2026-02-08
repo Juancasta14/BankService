856e43a528e354f1defce386dde859dcb128664f
Juan David <juanfelipe9902@gmail.com>
2026-02-08 16:50:46 -0500

    arregrando imports

--- App/fastapi_app/adapters/outbound/persistence/sqlalchemy/account_repository_sqlalchemy.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from adapters.outbound.persistence.sqlalchemy.models import AccountDB
-
-class AccountsRepositorySqlAlchemy:
-    def __init__(self, db: Session):
-        self.db = db
-
-    def list_by_customer(self, customer_id: int):
-        return self.db.query(AccountDB).filter(AccountDB.customer_id == customer_id).all()
